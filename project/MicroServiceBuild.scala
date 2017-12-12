cda9eaafed91b8000479f7ffb9371de9d6b5fd78
calinursaciuc <calin.ursaciuc@digital.hmrc.gov.uk>
2017-12-12 16:19:20 +0000

    VOAC-STUB: WIP

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -9,7 +9,8 @@ object MicroServiceBuild extends Build with MicroService {
 
   val appName = "voa-contact-data-transfer-stub"
 
-  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+  import play.sbt.PlayImport._
+  import play.core.PlayVersion
 
   private val playHealthVersion = "2.1.0"
   private val logbackJsonLoggerVersion = "3.1.0"
@@ -24,23 +25,35 @@ object MicroServiceBuild extends Build with MicroService {
   private val playReactivemongoVersion = "5.2.0"
   private val playConditionalFormMappingVersion = "0.2.0"
   private val playLanguageVersion = "3.4.0"
-  private val bootstrapVersion = "1.0.0"
+  private val bootstrapVersion = "1.1.0"
   private val reactiveMongoVersion = "6.1.0"
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ Test()
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.1.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-reactivemongo" % reactiveMongoVersion,
     "com.eclipsesource" %% "play-json-schema-validator" % "0.8.9"
   )
 
-  def test(scope: String = "test,it") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-    "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % scope,
-    "org.pegdown" % "pegdown" % pegdownVersion % scope,
-    "org.jsoup" % "jsoup" % "1.10.3" % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.mockito" % "mockito-all" % mockitoAllVersion % scope
-  )
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+        "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % scope,
+        "org.pegdown" % "pegdown" % pegdownVersion % scope,
+        "org.jsoup" % "jsoup" % "1.10.3" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "org.mockito" % "mockito-all" % mockitoAllVersion % scope
+      )
+    }.test
+  }
 
 }
